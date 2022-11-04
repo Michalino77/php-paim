5500ee52ac5f2a0975e4f884b41725ca6551d668
michal <abc@example.com>
2022-11-04 20:36:50 +0100

    Initaial commit

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
 
-
-declare(strict_types=1);
-
-namespace App;
-
-// to powinno być na wersji na świat. Dla nas błędy są ważne.
-// error_reporting(0);
-// ini_set('display_errors', '0');
-
-require_once('./src/Controller.php');
-include_once('./src/utils/debug.php');
-
-$controller = new Controller($_GET, $_POST);
-$controller->run();
+echo "TEST";
